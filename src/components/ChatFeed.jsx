13e72f7a04887b69bf4b0fb74f0f10dc54dea589
mart2164 <mart2164@purdue.edu>
2024-05-13 00:12:42 -0400

    made changes

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,48 +4,48 @@ import MessageForm from './MessageForm';
 
 // Define the ChatFeed component with props passed in
 const ChatFeed = (props) => {
-  // Destructure necessary properties from props
-  const { chats, activeChat, userName, messages } = props;
+    // Destructure necessary properties from props
+    const { chats, activeChat, userName, messages } = props;
 
-  // Get the current chat object using the activeChat identifier
-  const chat = chats && chats[activeChat];
+    // Get the current chat object using the activeChat identifier
+    const chat = chats && chats[activeChat];
 
-  // Function to render read receipts based on whether the message has been read by each chat participant
-  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    // Function to render read receipts based on whether the message has been read by each chat participant
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
     <div
-      key={`read_${index}`}
-      className="read-receipt"
-      style={{
-        float: isMyMessage ? 'right' : 'left',
-        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
-      }}
+        key={`read_${index}`}
+        className="read-receipt"
+        style={{
+            float: isMyMessage ? 'right' : 'left',
+            backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+        }}
     />
   ));
 
-  // Function to render messages in the chat
-  const renderMessages = () => {
+    // Function to render messages in the chat
+    const renderMessages = () => {
 
     // Extract keys from messages object
     const keys = Object.keys(messages);
 
     return keys.map((key, index) => {
-      // Get message by key
-      const message = messages[key];
-      // Determine the key of the last message
-      const lastMessageKey = index === 0 ? null : keys[index - 1];
-      // Check if the current user sent the message 
-      const isMyMessage = userName === message.sender.username;
+        // Get message by key
+        const message = messages[key];
+        // Determine the key of the last message
+        const lastMessageKey = index === 0 ? null : keys[index - 1];
+        // Check if the current user sent the message 
+        const isMyMessage = userName === message.sender.username;
 
-      return (
+    return (
         <div key={`msg_${index}`} style={{ width: '100%' }}>
-          <div className="message-block">
+            <div className="message-block">
             {isMyMessage
               ? <MyMessage message={message} />
               : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
-          </div>
-          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+            </div>
+            <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
             {renderReadReceipts(message, isMyMessage)}
-          </div>
+            </div>
         </div>
       );
     });
